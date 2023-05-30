6e0bd1f2769e6c122a371b75c0bba0ae9d053ab8
Sean Delaney <sean@solspace.com>
2023-05-30 12:05:57 +0100

    Basic demo setup

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Form from './components/Form.vue'
+</script>
+
+<template>
+    <main class="flex min-h-screen flex-col items-center justify-center p-24">
+        <h2 class="mb-3 text-2xl font-semibold">VueJS + GraphQL demo for Freeform 4.x for Craft</h2>
+        <Form />
+    </main>
+</template>
+
+<style scoped>
+</style>
