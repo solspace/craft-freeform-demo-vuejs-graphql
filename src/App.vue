43f980533047e62255876cca944888c9a8053e4d
kjmartens <kelsey@solspace.com>
2023-06-08 15:17:59 -0500

    Switching form and further cleanup

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import ContactForm from './ContactForm.vue';
 
 <template>
     <main class="flex min-h-screen flex-col items-left justify-left p-24">
-        <h2 class="mb-3 text-2xl font-semibold">VueJS + GraphQL demo for Freeform 4.x for Craft</h2>
+        <h2 class="mb-3 text-2xl font-semibold text-left">VueJS + GraphQL demo for Freeform 4.x for Craft</h2>
         <ContactForm />
     </main>
 </template>
