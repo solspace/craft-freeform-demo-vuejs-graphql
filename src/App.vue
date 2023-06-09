d5d8a16051c51d6fca74844c4058818d59ddf8b1
kjmartens <kelsey@solspace.com>
2023-06-09 09:59:35 -0500

    Tweak

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import QuoteForm from './QuoteForm.vue';
 </script>
 
 <template>
-    <main class="flex min-h-screen flex-col items-left justify-left p-10">
+    <main class="flex min-h-screen flex-col items-left justify-left p-5">
         <h2 class="mb-1 text-lg font-semibold text-left">Freeform 4 for Craft 4</h2>
         <h1 class="mb-5 pb-2 text-2xl font-semibold text-left">VueJS + GraphQL demo</h1>
         <QuoteForm />
